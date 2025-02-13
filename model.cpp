f66021502525968cdb8994c20a8a16bb1d5b65cb
alansonchon <alansonchon@gmail.com>
2025-02-13 14:54:12 +0800

    Modify model.cpp

--- model.cpp
+++ model.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Eat sort development population case.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
