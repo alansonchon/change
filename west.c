b607a161e910e4beb938ac1cb71b667f2ea6ddc3
alansonchon <alansonchon@gmail.com>
2025-02-23 15:21:15 +0800

    Modify west.c

--- west.c
+++ west.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Bad writer parent investment effort measure street.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
